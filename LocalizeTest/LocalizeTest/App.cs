335e7a962fe7b067cbad0fe57d9066c90e44193c
punio <punio@outlook.com>
2014-06-12 15:58:15 +0900

    Sample

---
+++ LocalizeTest/LocalizeTest/App.cs
@@ -0,0 +1,33 @@
+﻿using Xamarin.Forms;
+
+namespace LocalizeTest
+{
+	public class App
+	{
+		public static Page GetMainPage()
+		{
+			return new XamlPage();
+			return new ContentPage
+			{
+				Content = new StackLayout
+				{
+					Children = {
+						new Label
+						{
+//							Text = "Hello, Forms !",
+							Text = Strings.Hello,
+							VerticalOptions = LayoutOptions.CenterAndExpand,
+							HorizontalOptions = LayoutOptions.CenterAndExpand,
+						},
+						new Label
+						{
+							Text = System.Globalization.CultureInfo.CurrentCulture.ToString(),
+							VerticalOptions = LayoutOptions.CenterAndExpand,
+							HorizontalOptions = LayoutOptions.CenterAndExpand,
+						},
+					},
+				}
+			};
+		}
+	}
+}
---
+++ LocalizeTest/LocalizeTest/LocalizedStringExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace LocalizeTest
+{
+	[ContentProperty("Text")]
+	public class LocalizedStringExtension : IMarkupExtension
+	{
+		public string Text { get; set; }
+
+		public object ProvideValue(IServiceProvider serviceProvider)
+		{
+			if (Text == null) return null;
+			return Strings.ResourceManager.GetString(Text);
+		}
+	}
+}
